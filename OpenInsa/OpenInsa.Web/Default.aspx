2d11a7932989d5e7ae710be4db2e885cbaee9ef8
tintin00 <minkook.kim@gmail.com>
2013-11-27 20:14:26 +0900

    ... 3456789

---
+++ OpenInsa/OpenInsa.Web/Default.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EntLib.iBatis.Default" %>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <style type="text/css">
+
+TABLE {
+	SCROLLBAR-FACE-COLOR: #f7f7f7; FONT-SIZE: 9pt; SCROLLBAR-HIGHLIGHT-COLOR: #ffffff; SCROLLBAR-SHADOW-COLOR: #cccccc; COLOR: #585858; SCROLLBAR-3DLIGHT-COLOR: #ffffff; LINE-HEIGHT: 18px; SCROLLBAR-ARROW-COLOR: #cccccc; SCROLLBAR-TRACK-COLOR: #ffffff; FONT-FAMILY: "돋움", "Seoul", "arial", "Verdana", "helvetica"; SCROLLBAR-DARKSHADOW-COLOR: #ffffff;
+	margin-right: 1px;
+}
+TR {
+	SCROLLBAR-FACE-COLOR: #f7f7f7; FONT-SIZE: 9pt; SCROLLBAR-HIGHLIGHT-COLOR: #ffffff; SCROLLBAR-SHADOW-COLOR: #cccccc; COLOR: #585858; SCROLLBAR-3DLIGHT-COLOR: #ffffff; LINE-HEIGHT: 18px; SCROLLBAR-ARROW-COLOR: #cccccc; SCROLLBAR-TRACK-COLOR: #ffffff; FONT-FAMILY: "돋움", "Seoul", "arial", "Verdana", "helvetica"; SCROLLBAR-DARKSHADOW-COLOR: #ffffff
+}
+TD {
+	SCROLLBAR-FACE-COLOR: #f7f7f7; FONT-SIZE: 9pt; SCROLLBAR-HIGHLIGHT-COLOR: #ffffff; SCROLLBAR-SHADOW-COLOR: #cccccc; COLOR: #585858; SCROLLBAR-3DLIGHT-COLOR: #ffffff; LINE-HEIGHT: 18px; SCROLLBAR-ARROW-COLOR: #cccccc; SCROLLBAR-TRACK-COLOR: #ffffff; FONT-FAMILY: "돋움", "Seoul", "arial", "Verdana", "helvetica"; SCROLLBAR-DARKSHADOW-COLOR: #ffffff
+}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table id="LoginForm" runat="server" border="0" cellpadding="0" cellspacing="3">
+            <tr>
+                <td>
+                    <h5>
+                    </h5>
+                    <h5>
+                        &nbsp;</h5>
+                    <h5>
+                        &nbsp;</h5>
+                    <h5>
+                        <b>&quot;로그인 하세요&quot;</b></h5>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Your Name :<br />
+                    <asp:TextBox ID="txtName" value="mkkim" runat="server" Height="20px" Width="200px"></asp:TextBox>
+                    &nbsp;
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Your Password :<br />
+                    <asp:TextBox ID="txtPwd" value="1111" runat="server" Height="20px" TextMode="Password" 
+                        Width="200px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnLoginOK" runat="server" Text="OK" 
+                        Width="60px" onclick="btnLoginOK_Click" />
+                    <asp:Button ID="Cancle_btn" runat="server" Text="Cancle" Width="60px" />
+                    <asp:Button ID="Button1" runat="server" Text="Regidit" 
+                        Width="60px" onclick="Button1_Click" />
+                    <br />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    <table id="loginchk" runat="server">
+        <tr>
+            <td>
+                <asp:Button ID="btn_enterCancel" runat="server" BackColor="GreenYellow" 
+                    Height="0px" OnClientClick="return false;" Text="" Width="0px" />
+                <asp:Label ID="namelbl2" runat="server" ForeColor="#333333"></asp:Label>
+                님 로그인
+                <asp:Button ID="btnLogOut" runat="server" style="height: 21px" TabIndex="1000" 
+                    Text="LogOut" onclick="btnLogOut_Click" />
+                &nbsp;<br />
+                <br />
+                <asp:Button ID="GotoBoar_btn" runat="server" Text="게시판 보러가기" 
+                    onclick="GotoBoar_btn_Click" Width="178px" />&nbsp;&nbsp;</td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
+
---
+++ OpenInsa/OpenInsa.Web/Default.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//네임스페이스 추가 
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EntLib.iBatis
+{
+    public partial class Default : PageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.UserID != null)
+            {
+                namelbl2.Text = this.UserID + "님 정상 로그인 되었습니다.";
+
+                //로그인 테이블 안보이게
+                LoginForm.Visible = false;
+
+                //세션 있다면 로그인된 상태 보이게
+                loginchk.Visible = true;
+
+
+            }
+            else
+            {
+                loginchk.Visible = false;
+            }
+
+
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("./Registration.aspx");
+        }
+        protected void btnLoginOK_Click(object sender, EventArgs e)
+        {
+            Profile profile = new Profile();
+            profile.MEMBER_NAME = txtName.Text;
+            profile.MEMBER_PWD = txtPwd.Text;
+
+            IList<Profile> profileResult = mapper.QueryForList<Profile>("SelectProfile", profile);
+
+            if (profileResult.Count <= 0)
+            {
+                ClientScriptManager cs = Page.ClientScript;
+                cs.RegisterStartupScript(this.GetType(), "windowClose", "window.location.replace('./Default.aspx');", true);
+                cs.RegisterStartupScript(this.GetType(), "PopupScript", "alert('잘못된 ID/PW 입니다..');", true);
+            }
+            else
+            {
+                
+                //IList<Profile> profileResult = mapper.QueryForList<Profile>("SelectProfile", profile);
+                // 로그인 시에 가져온 class에 필요한 정보가 다 있으므로...
+                // 한번 더 DB Select 할 필요 없음.
+
+                //세션 추가 
+                Session.Add("UserID", txtName.Text);
+                Session.Add("UserName", profileResult[0].MEMBER_NAME);
+                Session.Add("UserPwd", profileResult[0].MEMBER_PWD);
+
+                if (Session["UserID"] != null)
+                {
+                    namelbl2.Text = Session["UserID"].ToString() + "님 정상 로그인 되었습니다.";
+
+                    //로그인 테이블 안보이게
+                    LoginForm.Visible = false;
+
+                    //센서 있다면 로그인된 상태 보이게
+                    loginchk.Visible = true;
+                }
+            }
+
+            #region 기존 소스
+            //string connection = ConfigurationManager.AppSettings["connectionString"];
+            //SqlConnection dbCon = new SqlConnection(connection);
+            //string sql = "SELECT seq FROM Profile WHERE (MEMBER_NAME = @userid) AND (MEMBER_PWD = @pwd)";
+            //SqlCommand dbCmd = new SqlCommand(sql, dbCon);
+            //dbCmd.Parameters.Add("@userid", System.Data.SqlDbType.VarChar, 50, "MEMBER_NAME");
+            //dbCmd.Parameters.Add("@pwd", System.Data.SqlDbType.NVarChar, 30, "MEMBER_PWD");
+
+            //dbCon.Open();
+            //dbCmd.Parameters["@userid"].Value = txtName.Text;
+            //dbCmd.Parameters["@pwd"].Value = txtPwd.Text;
+
+
+            //if (dbCmd.ExecuteScalar() == null)
+            //{
+
+            //    ClientScriptManager cs = Page.ClientScript;
+            //    cs.RegisterStartupScript(this.GetType(), "windowClose", "window.location.replace('./Default.aspx');", true);
+            //    cs.RegisterStartupScript(this.GetType(), "PopupScript", "alert('잘못된 ID/PW 입니다..');", true);
+
+
+            //    dbCmd.ExecuteNonQuery();
+            //    dbCon.Close();
+            //}
+            //else
+            //{
+
+            //    dbCmd.CommandText = "select seq, MEMBER_NAME, MEMBER_PWD, EMAIL from Profile where (MEMBER_NAME = @userid)";
+            //    SqlDataReader reader = dbCmd.ExecuteReader();
+            //    reader.Read();
+
+            //    //세션 추가 
+            //    Session.Add("UserID", txtName.Text);
+            //    Session.Add("UserName", reader["MEMBER_NAME"].ToString());
+            //    Session.Add("UserPwd", reader["MEMBER_PWD"].ToString());
+
+            //    reader.Close();
+            //    dbCmd.ExecuteNonQuery();
+            //    dbCon.Close();
+
+
+            //    if (Session["UserID"] != null)
+            //    {
+            //        namelbl2.Text = Session["UserID"].ToString() + "님 정상 로그인 되었습니다.";
+
+            //        //로그인 테이블 안보이게
+            //        LoginForm.Visible = false;
+
+            //        //센서 있다면 로그인된 상태 보이게
+            //        loginchk.Visible = true;
+            //    }
+            //} 
+            #endregion
+        }
+        protected void btnLogOut_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect(Request.RawUrl);
+        }
+        protected void GotoBoar_btn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("./Board_list.aspx");
+        }
+    }
+}
